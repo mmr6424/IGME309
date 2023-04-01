0e212bebfe5b9697b801f0547c26d814eeebfb0b
Rose Rushton <mmr6424@rit.edu>
2023-03-31 21:03:20 -0400

    added implementation to move correctly

--- HW03_Camera/MyCamera.cpp
+++ HW03_Camera/MyCamera.cpp
@@ -5,7 +5,10 @@ void MyCamera::SetPositionTargetAndUpward(vector3 a_v3Position, vector3 a_v3Targ
 {
 	//TODO:: replace the super call with your functionality
 	//Tip: Changing any positional vector forces you to calculate new directional ones
-	super::SetPositionTargetAndUpward(a_v3Position, a_v3Target, a_v3Upward);
+	//super::SetPositionTargetAndUpward(a_v3Position, a_v3Target, a_v3Upward);
+	m_v3Position = a_v3Position;
+	m_v3Target = a_v3Target;
+	m_v3Upward = a_v3Upward;
 
 	//After changing any vectors you need to recalculate the MyCamera View matrix.
 	//While this is executed within the parent call above, when you remove that line
@@ -21,8 +24,10 @@ void MyCamera::MoveForward(float a_fDistance)
 	//		 in the _Binary folder you will notice that we are moving 
 	//		 backwards and we never get closer to the plane as we should 
 	//		 because as we are looking directly at it.
-	m_v3Position -= vector3(0.0f, 0.0f, a_fDistance);
-	m_v3Target -= vector3(0.0f, 0.0f, a_fDistance);
+	m_v3Forward = glm::normalize(m_v3Target - m_v3Position);
+	m_v3Position += m_v3Forward * a_fDistance;
+	m_v3Target += m_v3Forward * a_fDistance;
+	CalculateView();
 
 	/*// Rotating a vector around a quat
 	quaternion q1 = glm::angleAxis(glm::radians(90.0f), AXIS_Y);
@@ -32,14 +37,16 @@ void MyCamera::MoveForward(float a_fDistance)
 void MyCamera::MoveVertical(float a_fDistance)
 {
 	//Tip:: Look at MoveForward
-	m_v3Position += vector3(0.0f, a_fDistance, 0.0f);
-	m_v3Target += vector3(0.0f, a_fDistance, 0.0f);
+	m_v3Upward = glm::cross(m_v3Rightward, m_v3Forward);
+	m_v3Position += m_v3Upward * a_fDistance;
+	m_v3Target += m_v3Upward * a_fDistance;
 }
 void MyCamera::MoveSideways(float a_fDistance)
 {
 	//Tip:: Look at MoveForward
-	m_v3Position += vector3(a_fDistance, 0.0f, 0.0f);
-	m_v3Target += vector3(a_fDistance, 0.0f, 0.0f);
+	m_v3Rightward = glm::cross(m_v3Forward, vector3(0, 1, 0));
+	m_v3Position += m_v3Rightward * a_fDistance;
+	m_v3Target += m_v3Rightward * a_fDistance;
 }
 void MyCamera::CalculateView(void)
 {
@@ -50,6 +57,7 @@ void MyCamera::CalculateView(void)
 	//		 have change so you only need to focus on the directional and positional 
 	//		 vectors. There is no need to calculate any right click process or connections.
 	m_m4View = glm::lookAt(m_v3Position, m_v3Target, m_v3Upward);
+
 }
 //You can assume that the code below does not need changes unless you expand the functionality
 //of the class or create helper methods, etc.
